3d5d4002c2ca0960868dae7574454cf2ac4b5827
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-12-28 12:26:23 +0300

    init

---
+++ server/src/server.js
@@ -0,0 +1,16 @@
+const http = require('http')
+
+const app = require('./app ')
+
+const server = http.createServer(app)
+const PORT = process.env.PORT || 8000;
+
+server.listen(PORT,()=>{
+    console.log(`Server listening on port ${PORT}`);
+})
+
+
+
+
+
+
