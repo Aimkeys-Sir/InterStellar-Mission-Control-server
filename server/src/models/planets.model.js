215d71553f49a99f10128e3fce37dc6010651c5c
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-12-28 18:07:28 +0300

    prepare planets data

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,3 +1,42 @@
-const planets = []
+const { parse } = require('csv-parse')
+const fs = require('fs')
+const path = require('path')
 
-module.exports = planets
+console.log(parse)
+const habitablePlanets = []
+
+function isHabitablePlanet(planet) {
+    return planet["soltype"] === 'Published Confirmed'
+        && planet["pl_insol"] > 0.36
+        && planet["pl_insol"] < 1.11
+        && planet["pl_rade"] < 1.6
+}
+
+function loadPlanetsData() {
+    return new Promise((resolve,reject) => {
+        
+        fs.createReadStream(path.join(__dirname,'..','..','data','exoplanets_data.csv'))
+            .pipe(parse({
+                comment: '#',
+                columns: true
+            }))
+            .on('data', data => {
+                if (isHabitablePlanet(data)) {
+                    habitablePlanets.push(data)
+                }
+            })
+            .on('error', err => {
+                reject(err)
+            })
+            .on('end', () => {
+                resolve()
+            })
+    })
+}
+
+
+
+module.exports = { 
+    loadPlanetsData,
+    planets: habitablePlanets 
+}
