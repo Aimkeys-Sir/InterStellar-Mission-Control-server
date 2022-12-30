1aaa3438d0a958c267ca57e2725015ec250ba798
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-12-30 17:31:47 +0300

    add clusters

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -28,7 +28,7 @@ function postLaunch(mission) {
     let ETA = calculateETA(planets.find(planet => planet.pl_name === mission["destination"]), mission["launch_date"])
     let launch = {
         ...mission,
-        mission_id: setMissionId(),
+        mission_id: setMissionId(mission.launch_date),
         created_at: new Date().toDateString(),
         arrival: "Future",
         upcoming: true,
@@ -40,10 +40,10 @@ function postLaunch(mission) {
     return launch
 }
 
-function setMissionId() {
+function setMissionId(launch_date) {
     let size = String(launches.size + 1)
     size = "0".repeat(3 - size.length) + size
-    return Number(new Date().getFullYear() + size)
+    return Number(new Date(launch_date).getFullYear() + size)
 }
 
 function calculateETA(planet, launchDate) {
