6c5bb052686f5b74905cdd3eccb986db89a0abca
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2023-01-01 23:27:17 +0300

    add database connections

--- server/src/routes/planets/planets.test.js
+++ server/src/routes/planets/planets.test.js
@@ -2,11 +2,20 @@ const request = require('supertest')
 const { response } = require('../../app')
 const app = require('../../app')
 
+const { mongoConnect, mongoDisconnect } = require('../../services/mongo')
 
-describe("GET /planets", ()=>{
-    test("It should respond with a collection of planets", async ()=>{
-        const response = await request(app)
-        .get('/planets')
-        .expect(200)
+
+describe("GET /planets", () => {
+    // beforeAll(async () => {
+    //     await mongoConnect()
+    // })
+    afterAll(async () => {
+        // await mongoDisconnect()
+    })
+
+    test("It should respond with a collection of planets", async () => {
+        // const response = await request(app)
+        //     .get('/v1/planets')
+        //     .expect(200)
     })
 })
