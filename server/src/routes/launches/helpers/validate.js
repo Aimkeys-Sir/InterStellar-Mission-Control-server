b4b9d724ffbda0100e5d57ceede777edad899514
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2023-01-01 23:29:04 +0300

    split error names

--- server/src/routes/launches/helpers/validate.js
+++ server/src/routes/launches/helpers/validate.js
@@ -1,5 +1,5 @@
 function validate(object, value){
-    if(!object[value]) return {error: `${value} must exist`}
+    if(!object[value]) return {error: `${value.split("_").join(" ")} must exist`}
 }
 function validateDate(dateString){
     const date = new Date(dateString).toDateString()
