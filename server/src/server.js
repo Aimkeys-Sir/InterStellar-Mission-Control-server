767e63ce8f5b70722dc49e7e1e78deebb22099d7
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2023-01-01 23:18:01 +0300

    refactor version

--- server/src/server.js
+++ server/src/server.js
@@ -2,13 +2,17 @@ const http = require('http')
 
 const app = require('./app')
 
+const {mongoConnect} = require('./services/mongo')
 const { loadPlanetsData } = require('./models/planets.model')
 
 const server = http.createServer(app)
 const PORT = process.env.PORT || 8000;
 
+
+
 async function startServer() {
-    await loadPlanetsData()
+    await mongoConnect()
+    // await loadPlanetsData()
 
     server.listen(PORT, () => {
         console.log(`Server listening on port ${PORT}`);
