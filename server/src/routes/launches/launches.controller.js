c4eeead7b08fa6aa420cd9c8b69ced57bbd14ff3
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2023-01-05 12:12:07 +0300

    add pagination

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,29 +1,52 @@
-const {getLaunches,postLaunch} = require('../../models/launches.model')
-const {validate,validateDate} = require('./helpers/validate')
+const { getLaunches, postLaunch, arbortLaunch, getHistory } = require('../../models/launches.model')
+const getPagination = require('./helpers/query')
+const { validate, validateDate } = require('./helpers/validate')
 
-async function httpGetLaunches(req,res){
-    return res.status(200).json(await getLaunches())
+
+async function httpGetLaunches(req, res) {
+    const { skip, limit } = getPagination(req.query)
+    const launches = await getLaunches(skip, limit)
+    return res.status(200).json(launches)
 }
-function httpPostLaunches(req,res){
+function httpPostLaunches(req, res) {
     const errors = []
     const mission = req.body
 
-    if(validateDate(mission.launch_date)){
+    if (validateDate(mission.launch_date)) {
         errors(validateDate(mission.launch_date))
     }
 
-    Object.keys(mission).forEach(key=>{
-        if(validate(mission,key)){
-            errors.push(validate(mission,key))
+    Object.keys(mission).forEach(key => {
+        if (validate(mission, key)) {
+            errors.push(validate(mission, key))
         }
     })
-    if(errors.length){
+    if (errors.length) {
         return res.status(422).json(errors)
     }
     return res.status(201).json(postLaunch(mission))
 }
 
+async function httpAbortLaunch(req, res) {
+    const launch = await arbortLaunch(req.body.mission_id)
+    if (launch) {
+        return res.status(204)
+    } else {
+        return res.status(400).json({ error: "Abort failed!!" })
+    }
+}
+
+async function httpGetHistory(req, res) {
+    const {skip,limit} = getPagination(req.query)
+    const history = await getHistory(skip,limit)
+
+
+    return res.status(200).json(history)
+}
+
 module.exports = {
     httpGetLaunches,
-    httpPostLaunches
+    httpPostLaunches,
+    httpAbortLaunch,
+    httpGetHistory
 }
