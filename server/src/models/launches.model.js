de9494a7e908035f661bd5552bb2487e043a8827
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2023-01-05 12:11:32 +0300

    load spaceX launches

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,32 +1,66 @@
+const axios = require('axios')
+
 const planets = require('./planets.mongo')
 const launchesDatabase = require('./launches.mongo')
 
 const launches = new Map()
 
-const launch = {
-    mission_id: 2022001,
-    launch_date: new Date(2024, 1, 24).toDateString(),
-    ETA: new Date(2027, 3, 4).toDateString(),
-    success: true,
-    mission_name: "Zoe one",
-    space_craft: "Falcon-9",
-    destination: "Kepler-442 b",
-    upcoming: true,
+
+const SPACEX_API_URL = "https://api.spacexdata.com/v4/launches/query"
+
+async function getLaunchFromDb(launch) {
+    return await launchesDatabase.findOne(launch)
 }
 
-// launches.set(launch.mission_id, launch)
-upsertLaunches(launch)
-
-async function upsertLaunches(launch) {
-
-    const planet = await planets.findOne({
-        pl_name: launch.destination
+async function getFromSpaceX() {
+    const res = await axios.post(SPACEX_API_URL, {
+        query: {},
+        options: {
+            pagination: false,
+            populate: [
+                {
+                    path: "rocket",
+                    select: {
+                        name: 1
+                    }
+                }
+            ]
+        }
     })
+    if (res.statusText === "OK") {
+        const launchDocs = res.data.docs
 
-    if (!planet) {
-        throw new Error("That planet, I don't know about!")
+        for (const launchDoc of launchDocs) {
+            const launch = {
+                mission_id: await setMissionId(launchDoc["date_utc"], launchDoc["flight_number"]),
+                launch_date: launchDoc["date_utc"],
+                mission_name: launchDoc["name"],
+                success: launchDoc["success"],
+                upcoming: launchDoc["upcoming"],
+                space_craft: launchDoc["rocket"]["name"]
+            }
+            upsertLaunches(launch)
+        }
+        console.log("SpaceX data loaded successfully")
+    }else{
+        console.error(`Error downloading spaceX data`)
+        throw new Error("Downloading data from spaceX failed.")
     }
 
+
+}
+
+async function loadLaunchesData() {
+    const firstLaunch = await getLaunchFromDb({ mission_id: 2006001, mission_name: "FalconSat" })
+    if (firstLaunch) {
+        console.log("SpaceX launches data already loaded!")
+        return
+    } else {
+        getFromSpaceX()
+    }
+}
+
+async function upsertLaunches(launch) {
     await launchesDatabase.updateOne({
         mission_id: launch.mission_id,
         mission_name: launch.mission_name
@@ -36,41 +70,69 @@ async function upsertLaunches(launch) {
     )
 }
 
-async function getLaunches() {
-    return await launchesDatabase.find({}, '-_id -__v')
+async function getLaunches(skip,limit) {
+    return await launchesDatabase
+    .find({upcoming: true}, '-_id -__v')
+    .sort({mission_id: 'desc'})
+    .skip(skip)
+    .limit(limit)
 }
 
-function postLaunch(mission) {
-    let ETA = calculateETA(planets.find(mission["destination"], mission["launch_date"]))
+async function postLaunch(mission) {
+    let ETA = await calculateETA(mission)
     let launch = {
         ...mission,
-        mission_id: setMissionId(mission.launch_date),
-        created_at: new Date().toDateString(),
-        arrival: "Future",
+        mission_id: await setMissionId(mission.launch_date),
+        success: true,
         upcoming: true,
         launch_date: new Date(mission.launch_date).toDateString(),
         ETA
+    }
+    const planet = await planets.findOne({
+        pl_name: launch.destination
+    })
+
+    if (!planet) {
+        throw new Error("That planet, I don't know about!")
     }
 
     upsertLaunches(launch)
     return launch
 }
 
-async function setMissionId(launch_date) {
-    let size = String((await launchesDatabase.find({})).length + 1)
+async function setMissionId(launch_date, flight_number = null) {
+    let size = flight_number || (await launchesDatabase.find({})).length + 1
+    size = String(size)
     size = "0".repeat(3 - size.length) + size
     return Number(new Date(launch_date).getFullYear() + size)
 }
 
-async function calculateETA(destination, launchDate) {
-    let dist = Number((await planets.find({ pl_name: destination })))['sy_dist'] || 10
+async function calculateETA(mission) {
+    let dist = Number((await planets.find({ pl_name: mission.destination })))['sy_dist'] || 10
+
 
     const time = dist * 365.25 * 24 * 60 * 60 * 1000
 
-    return new Date(Date.parse(launchDate) + time).toDateString()
+    return new Date(Date.parse(mission.launch_date) + time).toDateString()
+}
+
+async function arbortLaunch(mission_id) {
+    await launchesDatabase.updateOne({ mission_id }, { upcoming: false, success: false })
+    return await launchesDatabase.findOne({ mission_id })
+}
+
+async function getHistory(skip,limit){
+    return await launchesDatabase
+    .find({upcoming:false},'-__v -_id')
+    .sort({'mission_id': "desc"})
+    .skip(skip)
+    .limit(limit)
 }
 
 module.exports = {
+    loadLaunchesData,
     getLaunches,
-    postLaunch
+    postLaunch,
+    arbortLaunch,
+    getHistory
 }
--- server/src/server.js
+++ server/src/server.js
@@ -4,6 +4,7 @@ const app = require('./app')
 
 const {mongoConnect} = require('./services/mongo')
 const { loadPlanetsData } = require('./models/planets.model')
+const { loadLaunchesData } = require('./models/launches.model')
 
 const server = http.createServer(app)
 const PORT = process.env.PORT || 8000;
@@ -12,7 +13,8 @@ const PORT = process.env.PORT || 8000;
 
 async function startServer() {
     await mongoConnect()
-    // await loadPlanetsData()
+    await loadPlanetsData()
+    await loadLaunchesData()
 
     server.listen(PORT, () => {
         console.log(`Server listening on port ${PORT}`);
