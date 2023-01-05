4c103627c671134793c4f8d7bb99dbac0c50c6f3
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2023-01-05 12:12:32 +0300

    add hitory routes

--- server/src/routes/launches/launches.router.js
+++ server/src/routes/launches/launches.router.js
@@ -1,10 +1,16 @@
 const express = require('express')
 
-const {httpGetLaunches, httpPostLaunches} = require("./launches.controller")
+const {
+    httpGetLaunches,
+    httpPostLaunches,
+    httpAbortLaunch,
+    httpGetHistory } = require("./launches.controller")
 
 const launchesRouter = express.Router()
 
-launchesRouter.get('/',httpGetLaunches)
-launchesRouter.post('/',httpPostLaunches)
+launchesRouter.get('/', httpGetLaunches)
+launchesRouter.post('/', httpPostLaunches)
+launchesRouter.patch('/', httpAbortLaunch)
+launchesRouter.get('/history', httpGetHistory)
 
-module.exports= launchesRouter
+module.exports = launchesRouter
